bddbd8325e7b89b83838642ea231dcf0fa17da67
TappedIn <kruttik@tappedin.me>
2012-12-07 00:12:26 -0800

    Fixed page count issue.Refer https://github.com/elia/activeadmin-mongoid/issues/23

--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -2,6 +2,8 @@ module ActiveAdmin
   module Helpers
     module Collection
       def collection_size(collection=collection)
+        collection.count(true)
+        rescue
         collection.count
       end
     end
